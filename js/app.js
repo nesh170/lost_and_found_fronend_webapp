0cb4bd4979fcc99777cc44e7616abfbc997f87fe
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2016-11-24 00:24:01 -0500

    changed the log to

--- js/app.js
+++ js/app.js
@@ -33,48 +33,38 @@ app.run(['$rootScope','$log', function($rootScope,$log){
 app.run(['$rootScope','$location', function ($rootScope,$location) {
     $rootScope.$on('$locationChangeStart', function(){
         if($rootScope.checkLogin() == false){
-            $location.path("/")
+            $location.path("/");
         }
     })
 }]);
 
 
-app.controller('mainController',['$scope', '$location', '$rootScope', '$http',function ($scope, $location, $rootScope, $http) {
+app.controller('mainController',['$scope', '$location', '$rootScope', '$http','$log',function ($scope, $location, $rootScope, $http, $log) {
     $scope.logFunc = function() {
       console.log("in log functoin");
         if ($rootScope.loggedIn) {
           $location.path('/logout');
       }
       else {
-          window.location.href = "redirect.html?access_token=3cfddad3b540e5d5f70696731a325a0f288224c8&expires";
-            /*var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (xhttp.readyState == 4 && xhttp.status == 200) {
-                    console.log("successful get request made");
-                    console.log(xhttp.responseText);
-                }
-
-
-            };
-            xhttp.open("GET", "http://colab-sbx-122.oit.duke.edu:8080/loginsuccess?access_token=dc28f8048199a9272abd3ece7d7547c507ee4fd6", true);
-            xhttp.send();*/
-
-
-            $http({
-                method: 'GET',
-                url: 'http://colab-sbx-122.oit.duke.edu:8080/loginsuccess?access_token=3cfddad3b540e5d5f70696731a325a0f288224c8'
-                //params: {access_token: $rootScope.access_token}
-            }).then(function successCallback(response) {
-                console.log("successful endpoint call");
-                console.log(response.toString());
-            }, function errorCallback(response) {
-                console.log("There was an error with the endpoint");
-                console.log(response.status);
-                console.log(response.statusText);
-                console.log(response.data);
-                console.log(response);
-                console.log(response);
-            });
+          window.location.href = "redirect.html?access_token=393ea625a9c6cab09938340219ef3c75d44def81&expires";
       }
     };
+    $scope.successTest = function () {
+        $log.log("success Test function");
+        $http({
+            method: 'GET',
+            url: 'http://colab-sbx-122.oit.duke.edu:8080/loginsuccess',
+            params: {access_token: $rootScope.access_token}
+        }).then(function successCallback(response) {
+            $log.log("successful endpoint call");
+            $log.log(response.data);
+        }, function errorCallback(response) {
+            $log.log("There was an error with the endpoint");
+            $log.log(response.status);
+            $log.log(response.statusText);
+            $log.log(response.data);
+            $log.log(response);
+            $log.log(response);
+        });
+    }
 }]);
